9b1e99c2a4dcd4dfc6c8623c850da7283a8a6fa0
Nejc Schneider <nejc.sch@gmail.com>
2024-07-22 09:51:26 +0200

    imports

--- client/GetPrices.js
+++ client/GetPrices.js
@@ -1,1 +1,10 @@
 const ethers = require("ethers");
+
+const { 
+    addressFactory,
+    addressRouter,
+    addressFrom,
+    addressTo
+} = require("./AddressList");
+
+const { erc20ABI, factoryABI, routerABI, pairABI } = require("./AbiList");
