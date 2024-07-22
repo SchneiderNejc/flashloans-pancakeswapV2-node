b36f6800e41b404aa0c4eb60f3fc529b493334fd
Nejc Schneider <nejc.sch@gmail.com>
2024-07-22 10:55:41 +0200

    add getPrices()

--- client/GetPrices.js
+++ client/GetPrices.js
@@ -14,3 +14,6 @@ const provider = new ethers.providers.JsonRpcProvider("https://bsc-dataseed.bnbc
 const contractFactory = new ethers.Contract(addressFactory, factoryABI, provider);
 
 const contractRouter = new ethers.Contract(addressRouter, routerABI, provider);
+const getPrices = async (amountInHuman) => {
+const amountInHuman = "500";
+getPrices(amountInHuman);
