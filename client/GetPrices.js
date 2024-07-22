92485ec6313f9d62f4baac206cb77c3145645cb8
Nejc Schneider <nejc.sch@gmail.com>
2024-07-22 10:55:58 +0200

    convert the amount in

--- client/GetPrices.js
+++ client/GetPrices.js
@@ -15,5 +15,8 @@ const contractFactory = new ethers.Contract(addressFactory, factoryABI, provider
 
 const contractRouter = new ethers.Contract(addressRouter, routerABI, provider);
 const getPrices = async (amountInHuman) => {
+    const contractToken = new ethers.Contract(addressFrom, erc20ABI, provider);
+    const decimals = await contractToken.decimals(); // Read decimals from contract.
+    const amountIn = ethers.utils.parseUnits(amountInHuman, decimals).toString();
 const amountInHuman = "500";
 getPrices(amountInHuman);
