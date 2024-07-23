e9263efb6a2b6bf80878987b11f902ee601c5f54
Nejc Schneider <nejc.sch@gmail.com>
2024-07-23 11:55:01 +0200

    define ethers, provider, abi

--- uniswapV3/GetPrices.js
+++ uniswapV3/GetPrices.js
@@ -1,6 +1,8 @@
 // https://github.com/Uniswap/v3-periphery/blob/main/deploys.md
 // https://docs.uniswap.org/sdk/v3/guides/swaps/trading
 
+const ethers = require("ethers");
+const { abi: QuoterABI } = require("@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json");
 
 const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/a07ddfebd33a4161b915c09002291536");
 
