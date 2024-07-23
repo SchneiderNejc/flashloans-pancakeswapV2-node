5e8b588eba175f697f2a364ba74004693eff2616
Nejc Schneider <nejc.sch@gmail.com>
2024-07-23 11:54:06 +0200

    supply links to doccumentation

--- uniswapV3/GetPrices.js
+++ uniswapV3/GetPrices.js
@@ -1,3 +1,5 @@
+// https://github.com/Uniswap/v3-periphery/blob/main/deploys.md
+// https://docs.uniswap.org/sdk/v3/guides/swaps/trading
 
 
 const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/a07ddfebd33a4161b915c09002291536");
