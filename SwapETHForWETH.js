8a9add3bcb7ed9086fc60d87ae04a280874ca57d
Nejc Schneider <nejc.sch@gmail.com>
2024-07-24 17:11:07 +0200

    Provider and credentials declaration

--- SwapETHForWETH.js
+++ SwapETHForWETH.js
@@ -1,2 +1,10 @@
 const { ethers } = require('ethers');
 require('dotenv').config();
+
+// Sepolia provider
+const provider = new ethers.providers.JsonRpcProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
+
+// Credentials setup
+const address = `${process.env.ADDRESS}`;
+const privateKey = `${process.env.PRIVATE_KEY}`;
+const signer = new ethers.Wallet(privateKey, provider);
