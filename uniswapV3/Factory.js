5b8cba65448dcc65b3f297ce02c3798d76ce58c3
Nejc Schneider <nejc.sch@gmail.com>
2024-07-24 11:15:11 +0200

    getPool() function

--- uniswapV3/Factory.js
+++ uniswapV3/Factory.js
@@ -14,3 +14,13 @@ const factoryContract = new ethers.Contract(factory, abi, provider)
 const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const ANKR = "0x8290333ceF9e6D528dD5618Fb97a76f268f3EDD4";
 const fee = 3000;
+
+const getPool = async () => {
+    const pool = await factoryContract.getPool(WETH, ANKR, fee);
+    //console.log(pool);
+    return pool;
+}
+
+getPool();
+
+
