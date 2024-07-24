815b191f13a1db3ecd1180489536022ff1ced698
Nejc Schneider <nejc.sch@gmail.com>
2024-07-24 22:20:59 +0200

    rename address to sender

--- SwapETHForWETH.js
+++ SwapETHForWETH.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 const provider = new ethers.providers.JsonRpcProvider(`https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`);
 
 // Credentials setup
-const address = `${process.env.ADDRESS}`;
+const sender = `${process.env.ADDRESS}`;
 const privateKey = `${process.env.PRIVATE_KEY}`;
 const signer = new ethers.Wallet(privateKey, provider);
+
 const sendEth = async () => {
 
 sendEth();
